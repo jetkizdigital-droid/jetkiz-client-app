8b891fb33d374bcaa888703d016691dc85909dbb
jetkizdigital-droid <jetkizdigital@gmail.com>
2026-08-23 20:44:56 +0500

    feat: sync dynamic localization with app language

--- lib/core/localization/appLocalizationScope.dart
+++ lib/core/localization/appLocalizationScope.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:jetkiz_mobile/core/localization/appLanguage.dart';
 import 'package:jetkiz_mobile/core/localization/appStrings.dart';
+import 'package:jetkiz_mobile/core/localization/localizedValue.dart';
 
 class AppLocalizationScope extends InheritedWidget {
   final AppLanguage language;
   final ValueChanged<AppLanguage> onLanguageChanged;
 
-  const AppLocalizationScope({
+  AppLocalizationScope({
     super.key,
     required this.language,
     required this.onLanguageChanged,
     required super.child,
-  });
+  }) {
+    LocalizedValue.setLanguage(language);
+  }
 
   static AppLocalizationScope of(BuildContext context) {
     final scope =
         context.dependOnInheritedWidgetOfExactType<AppLocalizationScope>();
     assert(scope != null, 'AppLocalizationScope not found in widget tree');
-    return scope!;
+    LocalizedValue.setLanguage(scope!.language);
+    return scope;
   }
 
   AppStrings get strings => AppStrings(language);
 
   @override
   bool updateShouldNotify(AppLocalizationScope oldWidget) {
+    LocalizedValue.setLanguage(language);
     return oldWidget.language != language;
   }
 }
