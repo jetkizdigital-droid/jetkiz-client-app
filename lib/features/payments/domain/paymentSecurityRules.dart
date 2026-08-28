29eff95f635a5626caac9fd6d42c9a26b2dec527
jetkizdigital-droid <jetkizdigital@gmail.com>
2026-08-29 02:08:29 +0500

    chore(payments): remove unused foundation placeholders

--- lib/features/payments/domain/paymentSecurityRules.dart
+++
@@ -1,6 +0,0 @@
-abstract final class PaymentSecurityRules {
-  static const bool storeFullPan = false;
-  static const bool storeCvv = false;
-  static const bool trustClientReturnAsPaid = false;
-  static const bool exposeProviderSecretInClient = false;
-}
