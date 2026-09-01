2f95b8ab1d52bc9e71e1adaa431c7a8980817cf0
jetkizdigital-droid <jetkizdigital@gmail.com>
2026-09-01 17:59:52 +0500

    fix(ci): remove redundant dart:ui import

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,6 @@
   - каждый экран строится только после проверки реального backend ответа
 */
 
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
