9bf3f8a9cca7f53a38e37f7f77d99e6d3d3cc8a9
jetkizdigital-droid <jetkizdigital@gmail.com>
2026-09-24 22:04:03 +0600

    chore(release): bump client to 1.0.3+4

--- lib/core/config/appBuildInfo.dart
+++ lib/core/config/appBuildInfo.dart
@@ -1,7 +1,7 @@
 class AppBuildInfo {
   AppBuildInfo._();
 
-  static const versionName = '1.0.2';
-  static const buildNumber = '3';
+  static const versionName = '1.0.3';
+  static const buildNumber = '4';
   static const fullVersion = '$versionName+$buildNumber';
 }
